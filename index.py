7f36b2b67a7099232fd4379feed7fe6cc39b1652
kazungu <kazungu61@outlook.com>
2023-11-03 21:16:38 +0300

    specify the type of job to fetch

--- index.py
+++ index.py
@@ -12,8 +12,10 @@ results = soup.find(id="ResultsContainer")
 #print(results.prettify())
 
 job_elements = results.find_all('div', class_="card-content")
+python_jobs =results.find_all( "h2" , string = lambda text: "python" in text.lower())
+python_job_element = [h2_element.parent.parent.parent for h2_element in python_jobs]
 
-for job_element in job_elements:
+for job_element in python_job_element:
     title_element = job_element.find('h2', class_="title")
     company_element = job_element.find('h3', class_="company")
     location_element = job_element.find('p', class_ = "location")
