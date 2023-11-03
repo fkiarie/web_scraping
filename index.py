7b21a0b992a95292494945a65d9dc439cb80ffa8
kazungu <kazungu61@outlook.com>
2023-11-03 06:36:21 +0300

    Add BeautifulSoup lib

--- index.py
+++ index.py
@@ -1,6 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://realpython.github.io/fake-jobs/"
 page = requests.get(url)
 
-print(page.text)
+#print(page.text)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+results = soup.find(id="ResultsContainer")
